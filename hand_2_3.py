353ccaa427f841d38c0e05223286cfa226105076
5712131658 <156393204+5712131658@users.noreply.github.com>
2024-01-25 15:50:24 +0800

    Add files via upload

---
+++ hand_2_3.py
@@ -0,0 +1,35 @@
+import cv2 
+from hand_1_5 import HandDetector 
+webcam = cv2.VideoCapture(0) 
+webcam.set(3, 1_280)
+webcam.set(4, 720)
+detector = HandDetector(min_detection_confidence = 0.8) 
+shape_colour = colour_purple = (255, 0, 255) 
+colour_green = (0, 255, 0) 
+shape_centre_x = 100 
+shape_centre_y = 100 
+shape_width = 200 
+shape_height = 200 
+while True:
+  response, frame = webcam.read() 
+  frame = cv2.flip(frame, 1)
+  frame = detector.find_hands(frame = frame) 
+  list_landmarks = detector.find_position(frame = frame)
+  left_boundary = shape_centre_x - shape_width // 2
+  right_boundary = shape_centre_x + shape_width // 2
+  top_boundary = shape_centre_y - shape_height // 2
+  bottom_boundary = shape_centre_y + shape_height // 2
+  cv2.rectangle(frame,(left_boundary, top_boundary),(right_boundary, bottom_boundary),shape_colour,cv2.FILLED)
+  if list_landmarks != None and list_landmarks != []:
+    cursor = list_landmarks[8]
+    if left_boundary < cursor[1] < right_boundary and top_boundary < cursor[2] < bottom_boundary:
+      shape_colour = colour_green 
+      shape_centre_x = cursor[1]
+      shape_centre_y = cursor[2]
+    else:
+      shape_colour = colour_purple
+  cv2.imshow('webcam',frame)
+  if cv2.waitKey(1) & 0xFF == ord('*'):
+      break
+webcam.release()
+cv2.destroyAllWindows()
